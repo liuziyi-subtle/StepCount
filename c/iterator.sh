345c8e6d251cd6c4b3fbd53d2e37f399b867b094
Ziyi Liu <lc_lc2000@vip.163.com>
2021-04-01 21:49:56 +0800

    临时保存.

--- c/iterator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-FILES=~/Desktop/Lifesense/Gitlab/Step_algorithm/data/Benchmark/FalseSteps/*/*.csv
-for f in $FILES
-do
-  # take action on each file. $f store current file name
-  ./step $f
-done
